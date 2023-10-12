b8b4d964a8be9a46f613136bd9cd915bed4cfa27
TekerzRobotics45 <tekerz45@gmail.com>
2023-10-11 19:59:20 -0700

    Made Intake class skeleton. -Zoe B.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+public class Intake {
+   DcMotor intakeMotor;
+   private static final double MAX_SPEED = 0.5 ;
+
+   void pickUpPixel(){
+       //This line will pick up the pixel
+       intakeMotor.setPower(MAX_SPEED);
+
+
+   }
+
+
+}
