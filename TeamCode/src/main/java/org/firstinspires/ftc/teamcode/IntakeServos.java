b12632474c449e011d2cfc5d6ed8bca440a83d53
Alden <bitr8@outlook.com>
2024-10-22 20:01:20 -0700

    added a seperate servo controller for continuous rotation servos, because we need that, apparently

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeServos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeServos.java
@@ -1,30 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class IntakeServos {
 
     private final Gamepad gamepad;
-    ServoController leftIntakeServo  = new ServoController();
-    ServoController rightIntakeServo = new ServoController();
+    ContinuousServoController leftIntakeServo  = new ContinuousServoController();
+    ContinuousServoController rightIntakeServo = new ContinuousServoController();
 
     public IntakeServos(HardwareMap hardwareMap, Gamepad gamepad) {
         this.gamepad = gamepad;
         leftIntakeServo.init(hardwareMap, "intakeServoL");
         rightIntakeServo.init(hardwareMap, "intakeServoR");
+        leftIntakeServo.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightIntakeServo.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public void loop() {
         if (gamepad.right_trigger > 0) { //intake
-            leftIntakeServo.setServoPosition(1.0);
-            rightIntakeServo.setServoPosition(-1.0);
+            leftIntakeServo.setPower(1);
+            rightIntakeServo.setPower(1);
         } else if (gamepad.left_trigger > 0) { //ejection
-            leftIntakeServo.setServoPosition(-1.0);
-            rightIntakeServo.setServoPosition(1.0);
+            leftIntakeServo.setPower(-1);
+            rightIntakeServo.setPower(-1);
         } else {
-            leftIntakeServo.setServoPosition(0);
-            rightIntakeServo.setServoPosition(0);
+            leftIntakeServo.setPower(0);
+            rightIntakeServo.setPower(0);
         }
     }
 
